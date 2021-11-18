da5e7d6d322cac0c83b0af442449b5afebea011a
Prashanthglbkl <prashanthglbkl@gmail.com>
2021-11-18 13:11:05 +0530

    [prashanth] adding

---
+++ Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEHICAL
+{
+    class Car : Vehicals
+    {
+        public string modelName = "Mustang";  // Car field
+    }
+}
---
+++ Vehicals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEHICAL
+{
+    class Vehicals
+    {
+        public string brand = "Ford";  // Vehicle field
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+}
